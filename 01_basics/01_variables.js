375dd640ea9581be14437c45fc6d7c6632389b84
raman31191 <150108312+raman31191@users.noreply.github.com>
2026-03-26 08:33:47 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123456
+let accountEmail = "ramana@google.com"
+var accountPassword = "raman@31191_"
+accountCity = "Kanpur"
+let accountState;
+//accountId = 4
+console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
